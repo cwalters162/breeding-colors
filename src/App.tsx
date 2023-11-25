1673725bf03b21c473988c52b177e709702ba15d
Cherokee Walters <cherokeewalters162@gmail.com>
2023-11-25 17:23:37 -0500

    Added pairs of alleles

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,56 @@
-import {useEffect, useState} from 'react';
-
-enum Gene {
-    // Maybe make this an integer value
-    // for a real RGB to make a dynamic color system for cooler breading
-    RED = "R",
-    GREEN = "G",
-    BLUE = "B",
+import {useState} from 'react';
+enum Color {
+    RED = "R", // Red is Dominant over Blue
+    BLUE = "B", // Blue is Dominant over Green
+    GREEN = "G", // Green is Hardest to Get
 }
 
-type Shape = {
+type Allele = {
+    left: Color,
+    right: Color,
+}
+
+type Lifeform = {
     id: number
-    geneActive: Gene
-    geneRecessive: Gene
+    colorGene: Allele
 }
 
-function selectRandomGene(): Gene {
+function selectRandomPureColorPair(): Allele {
     const value = Math.floor(Math.random() * 4)
     switch (value) {
-        case 0: return Gene.RED;
-        case 1: return Gene.GREEN;
-        default: return Gene.BLUE;
+        case 0: return {
+            left: Color.BLUE,
+            right: Color.BLUE,
+        };
+        case 1: return {
+            left: Color.GREEN,
+            right: Color.GREEN,
+        };
+        default: return {
+            left: Color.RED,
+            right: Color.RED,
+        };
     }
-}
-
-function generateShapes(amount: number) {
-    const shapes: Shape[] = [];
-    for (let i = 0; i < amount; i++) {
-        shapes.push({
-            id: i,
-            geneActive: selectRandomGene(),
-            geneRecessive: selectRandomGene(),
-        })
-    }
-    return shapes
 }
 
 function App() {
-    const [parents, setParents] = useState<Shape[]>(generateShapes(10));
-    const [firstParent, setFirstParent] = useState<Shape | null>(null)
-    const [secondParent, setSecondParent] = useState<Shape | null>(null)
-    const [child, setChild] = useState<Shape | null>(null)
+    const [totalLifeforms, setTotalLifeforms] = useState(10)
+    const [parents, setParents] = useState<Lifeform[]>(generateLifeforms(10));
+    const [firstParent, setFirstParent] = useState<Lifeform | null>(null)
+    const [secondParent, setSecondParent] = useState<Lifeform | null>(null)
+    const [child, setChild] = useState<Lifeform | null>(null)
 
-    useEffect(() => {
-        console.log(firstParent)
-        console.log(secondParent)
-        console.log(child)
-    }, [child]);
+    function generateLifeforms(amount: number) {
+        const lifeforms: Lifeform[] = [];
+        for (let i = 0; i < amount; i++) {
+            lifeforms.push({
+                id: i,
+                colorGene: selectRandomPureColorPair(),
+            })
+        }
+
+        return lifeforms
+    }
 
     function handleParentOnClick(id: number) {
         if (firstParent === null) {
@@ -60,36 +65,26 @@ function App() {
     }
 
     function handleBreedOnClick() {
-        let childActiveGene = Gene.RED;
+        let left: Color = Color.RED;
+        let right: Color = Color.RED;
+        const FirstParentSideToInheritFrom = Math.floor(Math.random() * 2);
+        const SecondParentSideToInheritFrom = Math.floor(Math.random() * 2);
 
         if (!firstParent || !secondParent) {
             return
         }
 
-        if (firstParent.geneActive === secondParent.geneActive) {
-            //recessive gene might take over active gene
-
-            setChild({
-                id: firstParent.id + secondParent.id,
-                geneActive: firstParent.geneActive,
-                geneRecessive: firstParent.geneRecessive,
-            })
-        } else {
-            const parentToInheritFrom = Math.floor(Math.random() * 2);
-            console.log(parentToInheritFrom)
-            switch (parentToInheritFrom) {
-                case 0: childActiveGene = firstParent.geneActive; break;
-                case 1: childActiveGene = secondParent.geneActive; break;
-            }
-
-            //which recessive gene take over?
-
-            setChild({
-                id: firstParent.id + secondParent.id + 100,
-                geneActive: childActiveGene,
-                geneRecessive: firstParent.geneRecessive,
-            })
+        switch (FirstParentSideToInheritFrom) {
+            case 0: left = firstParent.colorGene.left; break;
+            case 1: left = firstParent.colorGene.right; break;
         }
+        switch (SecondParentSideToInheritFrom) {
+            case 0: right = secondParent.colorGene.right; break;
+            case 1: right = secondParent.colorGene.right; break;
+        }
+        const newChild = {id: totalLifeforms + 1, colorGene: {left, right},};
+        console.log(newChild.colorGene)
+        setChild(newChild);
     }
 
     return (
@@ -109,7 +104,7 @@ function App() {
                <h1>Parents</h1>
                <div className={"flex justify-center gap-4"}>
                {parents.map((parent)=> {
-                   const bgColor = getBackgroundColor(parent.geneActive);
+                   const bgColor = getBackgroundColor(parent.colorGene);
                    let selectedBorder = "";
 
                    if (parent.id === firstParent?.id || parent.id === secondParent?.id) {
@@ -125,17 +120,23 @@ function App() {
                 <h1>Child Result</h1>
                 {child && <div
                     key={child.id}
-                    className={`min-w-[2rem] min-h-[2rem] ${getBackgroundColor(child.geneActive)}-700`}/>}
+                    className={`min-w-[2rem] min-h-[2rem] ${getBackgroundColor(child.colorGene)}-700`}/>}
             </div>
    </div>
   )
 }
 
-function getBackgroundColor(gene: Gene) {
-    switch (gene) {
-        case Gene.RED: return "bg-red";
-        case Gene.GREEN: return "bg-green";
-        case Gene.BLUE: return "bg-blue";
+function getBackgroundColor(allele: Allele) {
+    const left = allele.left;
+    const right = allele.right;
+    if (left === Color.RED || right === Color.RED) {
+        return "bg-red";
+    }
+    if (left === Color.BLUE || right === Color.BLUE) {
+        return "bg-blue";
+    }
+    if (left === Color.GREEN && right === Color.GREEN) {
+        return "bg-green";
     }
 }
 
